08dcac77ede140d43587f292a6f71fece610a682
Semyon25 <49105052+Semyon25@users.noreply.github.com>
2024-12-09 21:04:54 +0000

    Исправление БД таблица users

--- db/users.py
+++ db/users.py
@@ -12,15 +12,17 @@ Session = sessionmaker(bind=engine)
 
 Base.metadata.create_all(engine)
 
+
 # Модель
 class User(Base):
   __tablename__ = 'users'
   id = Column(Numeric, primary_key=True, unique=True, nullable=False)
   username = Column(String, nullable=False)
-  tg_name = Column(String, nullable=False)
-  tg_surname = Column(String, nullable=False) 
-  created_date = Column(String, nullable=False) 
+  tg_name = Column(String)
+  tg_surname = Column(String)
+  created_date = Column(String, nullable=False)
   is_coffeemaker = Column(Integer, default=0)
+
 
 def get_user(user_id):
   session = Session()
@@ -28,11 +30,13 @@ def get_user(user_id):
   session.close()
   return current_user
 
+
 def get_user_by_username(username):
   session = Session()
   current_user = session.query(User).filter(User.username == username).first()
   session.close()
   return current_user
+
 
 def get_users():
   session = Session()
@@ -40,13 +44,19 @@ def get_users():
   session.close()
   return users
 
+
 def create_user(user_id, username, name, surname):
   now = datetime.datetime.now()
-  new_user = User(id=user_id, username=username, tg_name=name, tg_surname=surname, created_date=now.strftime("%d.%m.%Y %H:%M:%S"))
+  new_user = User(id=user_id,
+                  username=username,
+                  tg_name=name,
+                  tg_surname=surname,
+                  created_date=now.strftime("%d.%m.%Y %H:%M:%S"))
   session = Session()
   session.add(new_user)
   session.commit()
   session.close()
+
 
 def set_user_as_coffeemaker(user_id):
   session = Session()
