09bd92e3b35a83e46de39353a4b0e77cbbc229a2
Semyon25 <49105052+Semyon25@users.noreply.github.com>
2025-01-06 15:24:59 +0000

    Отправка сообщения конкретному пользователю

--- bot/broadcast_bot.py
+++ bot/broadcast_bot.py
@@ -3,12 +3,14 @@ from typing import List, Optional
 from aiogram import Bot
 from aiogram.fsm.state import StatesGroup, State
 from aiogram import Router
-from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InputMediaPhoto
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from aiogram.utils.media_group import MediaGroupBuilder
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
 from aiogram.exceptions import TelegramForbiddenError, TelegramBadRequest, TelegramRetryAfter
 from utils.admin import get_admin_id
-from db.users import get_users
+from db.users import User, get_user, get_users
+from utils.user_utils import get_user_name
 
 # Укажите задержку между отправками сообщений в секундах (чтобы не превысить лимиты Telegram)
 DELAY_BETWEEN_MESSAGES = 1
@@ -18,6 +20,17 @@ router = Router()
 class BroadcastState(StatesGroup):
   waiting_broadcast_content = State()
   waiting_confirmation = State()
+
+@router.message(Command('sendMessage'))
+async def handle_sendMessage_command(message: Message, state: FSMContext):
+  admin_id = int(get_admin_id())
+  if message.from_user.id == admin_id:
+    userId = message.text.split()[1]
+    user = get_user(userId)
+    if user is not None:
+      await state.update_data(user_id=user.id)
+      await message.answer('Отправьте текст и/или фотографии для рассылки:')
+      await state.set_state(BroadcastState.waiting_broadcast_content)
 
 
 @router.message(Command('broadcast'))
@@ -29,16 +42,14 @@ async def handle_broadcast_command(message: Message, state: FSMContext):
 
 
 @router.message(BroadcastState.waiting_broadcast_content)
-async def process_broadcast_text(message: Message, state: FSMContext):
+async def process_broadcast_content(message: Message, state: FSMContext):
   admin_id = int(get_admin_id())
   if message.from_user.id == admin_id:
-    data = await state.get_data()
-    text = data.get('broadcast_text')
-    photos = data.get('broadcast_photos', [])
-    if message.text:
-      text = message.text
-    if message.photo:
-      photos.extend([photo.file_id for photo in message.photo])
+    text = message.text
+    photos = []
+    if message.photo is not None:
+      photos = [photo.file_id for photo in message.photo]
+      text = message.caption
     if not text and not photos:
       await message.answer("Пожалуйста, отправьте текст или фотографии.")
       return
@@ -48,13 +59,10 @@ async def process_broadcast_text(message: Message, state: FSMContext):
         InlineKeyboardButton(text="Отменить", callback_data="cancel_broadcast")
     ]])
     if photos:
-      media = []
-      for i, photo in enumerate(photos):
-        if i == 0 and text:
-          media.append(InputMediaPhoto(media=photo, caption=text))
-        else:
-          media.append(InputMediaPhoto(media=photo))
-      await message.answer_media_group(media=media, reply_markup=keyboard)
+      media_builder = MediaGroupBuilder(caption=text)
+      media_builder.add_photo(media=photos[-1])
+      await message.answer_media_group(media=media_builder.build())
+      await message.answer('Отправить?', reply_markup=keyboard)
     elif text:
       await message.answer(text, reply_markup=keyboard)
     await state.update_data(broadcast_text=text, broadcast_photos=photos)
@@ -67,53 +75,47 @@ async def process_confirmation(query: CallbackQuery, state: FSMContext, bot: Bot
     data = await state.get_data()
     text = data.get('broadcast_text')
     photos = data.get('broadcast_photos')
-    await query.message.answer("Начинаю рассылку...")
-    errors = await send_message_to_all_users(bot, text, photos)
+    user_id = data.get('user_id')
+    await query.message.answer("Начинаю отправку...")
+    errors = await send_message_to_users(bot, user_id, text, photos)
     if errors:
       error_message = "Возникли следующие ошибки при рассылке:\n\n" + "\n".join(errors)
       await query.message.answer(error_message)
-    await query.message.answer('Рассылка завершена!')
+    await query.message.answer('Отправка завершена!')
     await query.answer()
   elif query.data == "cancel_broadcast":
-    await query.message.answer("Рассылка отменена.")
+    await query.message.answer("Отправка отменена.")
     await query.answer()
+  await query.message.edit_reply_markup(reply_markup=None) # Убираем кнопки
   await state.clear()
 
 
-async def send_message_to_all_users(bot: Bot, message_text: Optional[str], photos: Optional[List[str]] = None):
-    users = get_users()
+async def send_message_to_users(bot: Bot, userId: int, message_text: Optional[str], photos: Optional[List[str]] = None):
+    users = [get_user(userId)] if userId is not None else get_users()
     all_errors = []
-    for user in users:
-      # for test
-      if user.username != 'semyon_panichev' and user.username != 'TeacherEnglishDeutsch':
-        continue
-      
-      errors = await send_message_to_user(bot, user.id, message_text, photos)
+    for user in users:        
+      errors = await send_message_to_user(bot, user, message_text, photos, [])
       if errors:
         all_errors.extend(errors)
       await asyncio.sleep(DELAY_BETWEEN_MESSAGES)
     return all_errors
 
-async def send_message_to_user(bot: Bot, user_id: int, message_text: Optional[str], photos: Optional[List[str]] = None, errors: List[str] = []):
+async def send_message_to_user(bot: Bot, user: User, message_text: Optional[str], photos: Optional[List[str]], errors: List[str]):
   try:
-      if photos:
-          media = []
-          for i, photo in enumerate(photos):
-              if i == 0 and message_text:
-                  media.append(InputMediaPhoto(media=photo, caption=message_text))
-              else:
-                  media.append(InputMediaPhoto(media=photo))
-          await bot.send_media_group(user_id, media=media)
-      elif message_text:
-          await bot.send_message(user_id, message_text)
+    if photos:
+      media_builder = MediaGroupBuilder(caption=message_text)
+      media_builder.add_photo(media=photos[-1])
+      await bot.send_media_group(user.id, media=media_builder.build())
+    elif message_text:
+      await bot.send_message(user.id, message_text)
   except TelegramForbiddenError:
-    errors.append(f"Бот заблокирован пользователем {user_id}")
+    errors.append(f"Бот заблокирован пользователем {get_user_name(user)}")
   except TelegramBadRequest as e:
-    errors.append(f"Ошибка при отправке сообщения пользователю {user_id}: {e}")
+    errors.append(f"Ошибка при отправке сообщения пользователю {get_user_name(user)}: {e}")
   except TelegramRetryAfter as e:
-      await asyncio.sleep(e.retry_after)
-      # Повторяем отправку после ожидания
-      await send_message_to_user(bot, user_id, message_text, photos, errors)
+    await asyncio.sleep(e.retry_after)
+    # Повторяем отправку после ожидания
+    await send_message_to_user(bot, user, message_text, photos, errors)
   except Exception as e:
-    errors.append(f"Неизвестная ошибка при отправке сообщения пользователю {user_id}: {e}")
+    errors.append(f"Неизвестная ошибка при отправке сообщения пользователю {get_user_name(user)}: {e}")
   return errors
